c443b3914831c5a02461fa565ba3b7c9e86db446
llanesleonardo <llanesleonardo@gmail.com>
2022-10-13 00:57:56 -0700

    document.querySelector se usa para encontrar elementos con js dentro del documento HTML

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+
+const h1 = document.querySelector("h1");
+const parrf = document.querySelector("p");
+const pclase = document.querySelector(".parrafito");
+const pid = document.querySelector("#pid");
+const input = document.querySelector("input");
+
+console.log(input.value);
+
+console.log({
+  h1,
+  parrf,
+  pclase,
+  pid,
+  input,
+});
